3c20bd8d4b2d9c20d519fce83fbe9659d20964aa
ii <qb2main@gmail.com>
2023-06-29 14:50:31 +0300

    init

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import Example from './components/Example.vue';
+
+const app = createApp({
+    components: {
+        Example,
+    }
+})
+
+app.mount("#app");
---
+++ resources/js/components/Example.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1> Example</h1>
+</template>
